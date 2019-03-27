c6863bb651ba786ab8ed7c8603402bf959c2d3b3
Gerald <nephlin7@hotmail.com>
2019-03-26 20:22:06 -0600

    Set Google Analytics Track Id

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,8 +30,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
-        trackingId: "UA-XXXXXXXXX-X",
+        trackingId: "UA-137038166-1",
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
