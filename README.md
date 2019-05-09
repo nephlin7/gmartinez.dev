004193a1f9bb008218c397b4186ed3bbfc5ce4ce
nephlin7 <nephlin7@hotmail.com>
2019-05-09 16:03:01 -0600

    [Feat]: Some Changes

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ This is the front-end for my personal resume site
 gmartinez.dev
 
 Made with gatsby
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/01c66e84-e62e-4800-b41a-78544632324e/deploy-status)](https://app.netlify.com/sites/gmartinezdev/deploys)
---
+++ src/components/social-icons/social-icons.js
@@ -0,0 +1,53 @@
+import React from "react"
+import gitHubIcon from "../../images/github.svg"
+import twitterIcon from "../../images/twitter.svg"
+import linkedinIcon from "../../images/linkedin.svg"
+
+/** @jsx jsx */
+import { jsx } from "@emotion/core"
+
+const SocialIcons = () => {
+  return (
+    <>
+      <a
+        css={{
+          marginRight: 5,
+        }}
+        rel={`noopener noreferrer`}
+        target={`_blank`}
+        href={`https://github.com/nephlin7`}
+      >
+        <img title="GitHub" alt={`nephlin7`} width="30" src={gitHubIcon} />
+      </a>
+      <a
+        css={{
+          marginRight: 5,
+        }}
+        rel={`noopener noreferrer`}
+        target={`_blank`}
+        href={`https://twitter.com/GeraldM_92`}
+      >
+        <img
+          title="Follow me on twitter"
+          alt={`@GeraldM_92`}
+          width="30"
+          src={twitterIcon}
+        />
+      </a>
+      <a
+        rel={`noopener noreferrer`}
+        target={`_blank`}
+        href={`https://www.linkedin.com/in/gmartinez92/`}
+      >
+        <img
+          title="Linkedin"
+          alt={`gmartinez92`}
+          width="30"
+          src={linkedinIcon}
+        />
+      </a>
+    </>
+  )
+}
+
+export default SocialIcons
