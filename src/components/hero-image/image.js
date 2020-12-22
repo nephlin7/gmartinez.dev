8e885536ed5fbc401af83035bfa50da86d9f6b03
GeraldM <gerald@bejamas.io>
2020-12-22 10:43:52 -0600

    feat: add priority props to image

--- src/components/hero-image/image.js
+++ src/components/hero-image/image.js
@@ -3,12 +3,12 @@ import Image from "next/image"
 const DevImage = () => {
   return (
     <Image
-      loading="eager"
       className="devImage"
       alt="Gerald Martinez dev"
       width="518"
       height="600"
       src="/images/ger_dev_2020.jpg"
+      priority
     />
   )
 }
