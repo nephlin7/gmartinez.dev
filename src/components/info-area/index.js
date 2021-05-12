02027c2322f4199f0cfec29bcb5e56f4d3ff14c4
nephlin7 <gerald@bejamas.io>
2021-05-12 02:17:40 -0600

    feat: upgrade nextjs

--- src/components/info-area/index.js
+++ src/components/info-area/index.js
@@ -1,22 +1,29 @@
 import styles from "./info-area.module.scss"
+import ReactTyped from "react-typed"
 
 const InfoArea = () => {
   return (
     <section className={styles.infoAreaWrapper}>
-      <span>Front-end developer</span>
+      <span>Frontend developer</span>
       <h1 className={styles.trackingInExpand}>Gerald Martinez</h1>
       <p>
-        I'm a frontend developer based in Nicaragua with 4+ years of experience.
-        Currently working at <strong>Bejamas</strong> as a frontend developer,
-        building great stuff with
-        <a
-          rel="noreferrer noopener"
-          href={`https://twitter.com/hashtag/JAMstack?src=hash&lang=es`}
-          target={`_blank`}
-        >
-          #jamstack
-        </a>
-        technologies.
+        <ReactTyped
+          className="description"
+          loop={false}
+          typeSpeed={80}
+          backSpeed={20}
+          strings={[
+            "I'm a frontend developer based in Nicaragua with 4+ years of experience. Currently working at <strong>Bejamas</strong> as a frontend developer,  building great stuff with #jamstack technologies.",
+          ]}
+          smartBackspace
+          shuffle={false}
+          backDelay={1}
+          fadeOut
+          fadeOutDelay={100}
+          loopCount={0}
+          showCursor
+          cursorChar="|"
+        />
       </p>
 
       <br />
