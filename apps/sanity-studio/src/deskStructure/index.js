ec16dac7fdc0b290052d9a93ecd5df7c1a583432
Gerald <gerald@bejamas.io>
2022-07-07 17:00:08 -0600

    feat: latest changes

--- apps/sanity-studio/src/deskStructure/index.js
+++ apps/sanity-studio/src/deskStructure/index.js
@@ -16,28 +16,12 @@ export default () =>
                 .title('Site configuration')
                 .icon(MdSettings)
                 .child(S.document().title('Site configuration').schemaType('siteSettings').documentId('siteSettings')),
-               
             ]),
         ),
-        S.divider(),
-        S.listItem()
+      S.divider(),
+      S.listItem()
         .title('Front Page')
         .icon(MdHome)
-        .child(S.document().title('Site configuration').schemaType('frontPage').documentId('frontPage'),
-        ),
-       ...S.documentTypeListItems().filter(item => !['siteSettings', 'frontPage'].includes(item.getId()))
+        .child(S.document().title('Site configuration').schemaType('frontPage').documentId('frontPage')),
+      ...S.documentTypeListItems().filter((item) => !['siteSettings', 'frontPage'].includes(item.getId())),
     ]);
-
-
-
-export const getDefaultDocumentNode = (props) => {
-  /**
-   * Here you can define fallback views for document types without
-   * a structure definition for the document node. If you want different
-   * fallbacks for different types, or document values (e.g. if there is a slug present)
-   * you can set up that logic in here too.
-   * https://www.sanity.io/docs/structure-builder-reference#getdefaultdocumentnode-97e44ce262c9
-   */
-
-  return S.document().views([S.view.form()]);
-};
