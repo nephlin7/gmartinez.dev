ce91e045274003352f4997b15fcc29f3e77cb9e8
Gerald <gerald@bejamas.io>
2022-08-11 18:36:00 -0600

    fix: tweak analitycs

--- apps/web/src/pages/_document.jsx
+++ apps/web/src/pages/_document.jsx
@@ -15,21 +15,19 @@ export default class Document extends NextDocument {
           <Main />
           <NextScript />
           {process.env.NEXT_PUBLIC_ANALYTICS_ENV === 'production' && (
-            <>
-              <Script
-                id="googletagmanager-js"
-                strategy="lazyOnload"
-                src={`https://www.googletagmanager.com/gtag/js?id=${config?.gaTrackingId}`}
-              />
-              <Script id="gtag" strategy="beforeInteractive">
-                {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${config.gaTrackingId}');
-          `}
-              </Script>
-            </>
+            <Script
+              id="googletagmanager-js"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer', '${config.gaTrackingId}');
+  `,
+              }}
+            />
           )}
         </body>
       </Html>
