e7969e916c8e85ed38c8b4f5a2141edf167b732b
Gerald Martinez <gerald@bejamas.io>
2024-08-05 17:58:32 -0600

    chore: add type anotation

--- apps/web/src/sanity/client.ts
+++ apps/web/src/sanity/client.ts
@@ -1,10 +1,12 @@
-import {createClient} from 'next-sanity'
+import { createClient } from 'next-sanity';
 
-import {apiVersion, dataset, projectId} from './env'
+import type { SanityClient } from '@sanity/client';
 
-export const client = createClient({
+import { apiVersion, dataset, projectId } from './env';
+
+export const client: SanityClient = createClient({
   projectId,
   dataset,
   apiVersion, // https://www.sanity.io/docs/api-versioning
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
-})
+});
